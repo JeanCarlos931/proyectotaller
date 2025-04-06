c6efea53b92e851053dc9f9f2ec9dd1f883e68fc
JeanCarlos931 <j.huertas.3@estudiantec.cr>
2025-04-06 14:56:02 -0600

    registro completo

--- registro.py
+++ registro.py
@@ -5,16 +5,33 @@ with open("registro.txt", "r", encoding="utf-8") as registro:
     datos = json.load(registro)
 
 def login():
-    print("Iniciar sesión presionado")
+    user=input("Usurario: ").lower()
+    try:    #Si existe el archivo con el usuario entonces funciona y retorna false
+        with open(f"{user}_conversaciones.txt", "w", encoding="utf-8") as registro:
+            registro.close()
+        return(True)
+    except: #Si no existe da error el try y en el except crea el archivo retornando ademas un true
+        return(False)
 
-def register():
-    user=input("Usurario: ").lower
-    print(user)
-    with open(f"{user}_conversaciones.txt", "w", encoding="utf-8") as registro:
-        registro.close()
-    return(True)
+def register()->bool:
+    """Registra el usuario, si existe retorna un False indicando que ya está registrado ese usuario, si no existe retorna un True indicando que se registro correctamente
 
-register()
+    Returns:
+        bool: Retorna False si el usuario ya existe, si no un True de que se registró correctamente
+    """
+    
+    user=input("Usurario: ").lower()
+    try:    #Si existe el archivo con el usuario entonces funciona y retorna false
+        with open(f"{user}_conversaciones.txt", "r", encoding="utf-8") as registro:
+            registro.close()
+        return(False)
+    except: #Si no existe da error el try y en el except crea el archivo retornando ademas un true
+        with open(f"{user}_conversaciones.txt", "w", encoding="utf-8") as registro:
+            registro.close()
+        return(True)
+
+
+print(register())
 
 
 
